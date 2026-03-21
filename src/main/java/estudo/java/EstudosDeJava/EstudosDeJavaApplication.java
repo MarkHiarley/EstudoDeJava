9933e0e170135661edc597267d2fc6edcdf3e38e
MarkHiarley <hiarleycostamarley@gmail.com>
2026-03-20 21:21:56 -0300

    first commit

---
+++ src/main/java/estudo/java/EstudosDeJava/EstudosDeJavaApplication.java
@@ -0,0 +1,13 @@
+package estudo.java.EstudosDeJava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstudosDeJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstudosDeJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/estudo/java/EstudosDeJava/EstudosDeJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package estudo.java.EstudosDeJava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstudosDeJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
